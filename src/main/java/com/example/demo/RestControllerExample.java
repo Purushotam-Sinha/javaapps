79f6302d5030e63916fc1176c20661c3f59c9217
DELL <DELL@DESKTOP-V56N9R9>
2025-02-08 04:25:36 -0500

    Add controller into projects

---
+++ src/main/java/com/example/demo/RestControllerExample.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControllerExample {
+	
+	
+	@GetMapping("/")
+	public String getMessage()
+	{
+		return "<h2><font color='green'>Welcome Mr ."+new Date()+"</font></h2>";
+	}
+
+}
