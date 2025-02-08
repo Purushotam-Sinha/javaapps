d033344d90c2046d0ea43ecc80b0f29e3ba7ce94
Purushotam-Sinha <37238038+Purushotam-Sinha@users.noreply.github.com>
2025-02-08 02:00:00 -0500

    Create Welcome.java
    New Comment add in 8-Feb

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Welcome
+  {
+      public static void main(String as[])
+    {
+        System.out.println("Welcome to Java World ");
+      System.out.println("Date "+new Date());
+    }
+  }
