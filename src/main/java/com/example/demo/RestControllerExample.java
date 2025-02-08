e9a689b21db2d464207db715fe9cef3b30dd8bd4
DELL <DELL@DESKTOP-V56N9R9>
2025-02-08 04:40:29 -0500

    new message added into controller

--- src/main/java/com/example/demo/RestControllerExample.java
+++ src/main/java/com/example/demo/RestControllerExample.java
@@ -12,6 +12,7 @@ public class RestControllerExample {
 	@GetMapping("/")
 	public String getMessage()
 	{
+		System.out.println("Rest ApI called ");
 		return "<h2><font color='green'>Welcome Mr ."+new Date()+"</font></h2>";
 	}
 
